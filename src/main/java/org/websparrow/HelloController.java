1b054b7c47421ba00ae72214c275eca6ed31a1f1
Subhasish Pradhan <subhasish.manu@gmail.com>
2020-06-16 14:47:11 +0530

    initial commit

---
+++ src/main/java/org/websparrow/HelloController.java
@@ -0,0 +1,22 @@
+package org.websparrow;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+//	@RequestMapping("/hello.htm")
+//	public ModelAndView greet(@RequestParam("name") String name) throws Exception {
+//
+//		ModelAndView mv = new ModelAndView();
+//		
+//		// name of JSP page
+//		mv.setViewName("welcome");
+//
+//		// data that you want to show on JSP
+//		mv.addObject("name", name);
+//
+//		return mv;
+//	}
+
+}
